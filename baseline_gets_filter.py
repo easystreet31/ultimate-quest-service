9b32092bd24fef8c1075edbf0e0adb52d3923d44
easystreet31 <easystreet31@gmail.com>
2025-10-07 09:47:11 -0700

    Update baseline_gets_filter.py

--- baseline_gets_filter.py
+++ baseline_gets_filter.py
@@ -3,4 +3,5 @@ from starlette.requests import Request
 
 class BaselineGetsFilter(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        # Reserved for future: block accidental GETs to heavy routes, add request IDs, etc.
         return await call_next(request)
